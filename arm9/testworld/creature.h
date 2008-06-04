fd73e8060e034eb4823bf71eeeae238041f63d97
Jeremy Apthorp <nornagon@nornagon.net>
2008-06-04 06:38:54 +0000

    Remove testworld (as it no longer builds)

--- arm9/testworld/creature.h
+++
@@ -1,9 +0,0 @@
-#ifndef CREATURE_H
-#define CREATURE_H 1
-
-typedef struct creature_s {
-	int hp;
-	int ac;
-} creature_t;
-
-#endif /* CREATURE_H */
--- arm9/testworld/loadmap.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOADMAP_H
-#define LOADMAP_H 1
-
-#include "randmap.h"
-
-void load_map(map_t *map, size_t len, const char *desc);
-
-#endif /* LOADMAP_H */
