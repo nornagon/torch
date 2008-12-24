5f62c2ef333d960c7dfb911bef502735336ea12f
Jeremy Apthorp <nornagon@nornagon.net>
2008-12-24 23:56:22 +0000

    Add NDEBUG support to assert.h

--- arm9/include/assert.h
+++ arm9/include/assert.h
@@ -8,7 +8,17 @@
 extern "C" {
 #endif
 
+#if defined __cplusplus
+# define __ASSERT_VOID_CAST static_cast<void>
+#else
+# define __ASSERT_VOID_CAST (void)
+#endif
+
+#ifdef NDEBUG
+#define assert(k) (__ASSERT_VOID_CAST (0))
+#else
 #define assert(k) _assert(__FILE__,__LINE__,__func__,#k,(bool)(k))
+#endif
 
 void _assert(const char *, int, const char *, const char *, bool cond);
 
