19c3e1f5821432d03f19aaa1ca011d6c1d62cfa8
Jeremy Apthorp <nornagon@nornagon.net>
2009-01-03 02:48:58 +0000

    Update drawch such that it will compile under -Os; add comment pointing out dubious assumptions.

--- arm9/engine/draw.c
+++ arm9/engine/draw.c
@@ -28,22 +28,30 @@ void drawcq(u32 x, u32 y, u32 c, u32 color) {
 }
 
 void drawch(u16* mem, u32 x, u32 y, u32 c, u32 color) { // OPAQUE version (clobbers)
+	// FIXME: I'm not quite sure how to tell gcc that I want it to put the
+	// arguments to this in r{2..7} specifically. I assume that's what it'll do,
+	// since when I compiled with -Os it complained that there were no low
+	// registers in which it could put its arguments until I removed r{2..7} from
+	// the clobber list. I'd like to specify those registers in the clobber list
+	// (since I do in fact clobber them), but then gcc refuses to put the
+	// arguments in those registers. So here's hoping gcc doesn't do anything
+	// funny...
 	asm (
-			"adr r7, 2f\n"
-			"bx r7\n"
+			"adr r0, 2f\n"
+			"bx r0\n"
 			".align 4\n"
 			".arm\n"
 			"2:\n"
 			"sub %2, %2, #32\n"         // c -= ' '
 			"add %1, %0, %1, lsl #8\n"  // y = x+y*256
 			"orr r8, %3, #32768\n"      // r8 <- col | BIT(15)
-			"add r6, %5, %2, lsl #6\n"  // r6 <- &fontTiles[8*8*c]
-			"add r7, %4, %1, lsl #1\n"  // r7 <- &backbuf[y*256+x]
+			"add r11, %5, %2, lsl #6\n"  // r11 <- &fontTiles[8*8*c]
+			"add r12, %4, %1, lsl #1\n"  // r12 <- &backbuf[y*256+x]
 
 			"mov r9, #7\n"            // we want to draw 8 lines
 
 			"1:\n"
-			"ldmia r6!, {r4, r5}\n"   // grab 8 bytes from the font
+			"ldmia r11!, {r4, r10}\n"   // grab 8 bytes from the font
 
 			"eor r0, r0, r0\n"
 			"movs r4, r4, lsr #1\n"   // r4 >> 1, C flag set if that bit was 1
@@ -58,19 +66,19 @@ void drawch(u16* mem, u32 x, u32 y, u32 c, u32 color) { // OPAQUE version (clobb
 			"orrcs r1, r8, lsl #16\n"
 
 			"eor r2, r2, r2\n"
-			"movs r5, r5, lsr #1\n"
+			"movs r10, r10, lsr #1\n"
 			"movcs r2, r8\n"
-			"movs r5, r5, lsr #8\n"
+			"movs r10, r10, lsr #8\n"
 			"orrcs r2, r8, lsl #16\n"
 
 			"eor r3, r3, r3\n"
-			"movs r5, r5, lsr #8\n"
+			"movs r10, r10, lsr #8\n"
 			"movcs r3, r8\n"
-			"movs r5, r5, lsr #8\n"
+			"movs r10, r10, lsr #8\n"
 			"orrcs r3, r8, lsl #16\n"
 
-			"stm r7, {r0, r1, r2, r3}\n"
-			"add r7, r7, #512\n"
+			"stm r12, {r0, r1, r2, r3}\n"
+			"add r12, r12, #512\n"
 			"subs r9, r9, #1\n"
 			"bpl 1b\n"
 
@@ -81,6 +89,6 @@ void drawch(u16* mem, u32 x, u32 y, u32 c, u32 color) { // OPAQUE version (clobb
 
 			: /* no output */
 			: "r"(x), "r"(y), "r"(c), "r"(color), "r"(mem), "r"(fontTiles)
-			: "r0", "r1", "r5", "r6", "r7", "r8", "r9"
+			: "r0", "r1", "r10", "r11", "r12", "r8", "r9"
 			);
 }
