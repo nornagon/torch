a75208dead15d65e9214938beae707a1008da32d
Jeremy Apthorp <nornagon@nornagon.net>
2009-01-08 05:41:01 +0000

    Rearrange object.h; fix a memory leak in stack_item_push.

--- arm9/adrift/object.cpp
+++ arm9/adrift/object.cpp
@@ -5,29 +5,33 @@ Object::Object(u16 _type): type(_type), quantity(1), orientation(D_NONE)
 {
 }
 
-Object *addObject(s16 x, s16 y, u16 type) {
+Object *addObject(s16 x, s16 y, u16 type, u8 quantity) {
 	Object *on = new Object(type);
+	on->quantity = quantity;
 	stack_item_push(game.map.at(x,y)->objects, on);
 	return on;
 }
 
-void stack_item_push(List<Object> &container, Object *obj) {
+void stack_item_push(List<Object> &container, Object *&obj) {
 	Object *contobj = container.head();
 	for (; contobj; contobj = contobj->next()) {
 		if (contobj->type == obj->type && obj->desc()->stackable)
 			break;
 	}
 	if (contobj) {
-		if ((int)contobj->quantity + (int)obj->quantity > 255) {
-			int overflow = (int)contobj->quantity + (int)obj->quantity - 255;
+		int overflow = (int)contobj->quantity + (int)obj->quantity - 255;
+		if (overflow > 0) {
 			obj->quantity = overflow;
 			contobj->quantity = 255;
+			container.push(obj);
 		} else {
 			contobj->quantity += obj->quantity;
-			obj = 0;
+			delete obj;
+			obj = contobj;
 		}
+	} else {
+		container.push(obj);
 	}
-	if (obj) container.push(obj);
 }
 
 DataStream& operator <<(DataStream& s, Object &o)
--- arm9/adrift/object.h
+++ arm9/adrift/object.h
@@ -10,21 +10,34 @@
 struct Object : public listable<Object> {
 	POOLED(Object)
 
-	public:
-	Object(): type(0), quantity(1), orientation(D_NONE) {}
-	Object(u16 type);
-	const ObjectDesc* desc() { return &objectdesc[type]; }
+	public: // data members
 	u16 type;
 	u8 quantity;
 	DIRECTION orientation;
 
+	public: // functions
+	Object(): type(0), quantity(1), orientation(D_NONE) {}
+	Object(u16 type);
+
+	inline const ObjectDesc* desc() { return &objectdesc[type]; }
+
+	public: // serialisation
 	friend DataStream& operator <<(DataStream&, Object&);
 	friend DataStream& operator >>(DataStream&, Object&);
 };
 DataStream& operator <<(DataStream&, Object&);
 DataStream& operator >>(DataStream&, Object&);
 
-Object *addObject(s16 x, s16 y, u16 type);
-void stack_item_push(List<Object> &container, Object *obj);
+// Create and add an object to the map.
+Object *addObject(s16 x, s16 y, u16 type, u8 quantity = 1);
+// Push the given object into the given container. If the object can stack with
+// another object already in the container, increase the quantity of the
+// existing object and destroy the pushed object, updating the pointer passed
+// to point to the new stack. If stacking would cause overflow, stack up as
+// many as possible and split the remainder out into a new object in the bag
+// (in fact just decreases the quantity of the object you pass, while
+// increasing the quantity of the existing stack). If it can't stack, just
+// pushes the object into the container.
+void stack_item_push(List<Object> &container, Object *&obj);
 
 #endif /* OBJECT_H */
